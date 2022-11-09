d9e26ba276dd9cf8e94e58c31dd36c0774a898a4
odaialazzeh <odai.alazzeh@gmail.com>
2022-11-09 14:03:04 +0200

    Create modules folder

---
+++ modules/add.js
@@ -0,0 +1,31 @@
+const add = document.getElementById('add');
+
+const addfunction = (event) => {
+  if (event.key === 'Enter') {
+    let existingEntries = JSON.parse(localStorage.getItem('collection'));
+    const entrytask = document.getElementById('add').value;
+    if (existingEntries == null) existingEntries = [];
+    if (existingEntries) {
+      for (let i = 0; i <= existingEntries.length; i += 1) {
+        var task = {
+          description: entrytask,
+          completed: false,
+          index: i,
+        };
+      }
+    }
+
+    localStorage.setItem('task', JSON.stringify(task));
+    existingEntries.push(task);
+    localStorage.setItem('collection', JSON.stringify(existingEntries));
+    localStorage.removeItem('task');
+    if (add.value !== '') {
+      add.value = '';
+    }
+    window.location.reload();
+  }
+};
+
+add.addEventListener('keypress', addfunction);
+
+export default addfunction;
